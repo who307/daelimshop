8929ae6d374c02150cc75ef66c4a8478b27227df
who307 <who307@naver.com>
2020-06-15 09:40:39 +0900

    상세페이지 수정

--- client/src/Detail.js
+++ client/src/Detail.js
@@ -5,62 +5,109 @@ class Detail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productDetail: { 
+      productDetail: {
         title: "title(제목)ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ",
         price: "1,000(가격)",
         productState: "중고(상품상태)",
         trade: "교환불가",
         delivery: "택배 착불",
         area: "서울",
-        img: "/image/bird-932704_640.jpg"
+        img: "/image/bird-932704_640.jpg",
+        info: " 직거래 합니다 상태좋음 "
+    },sellerinfo : {
+        id : "who307",
+        name : "문건후",
+        department : "모바일인터넷",
+        schoolnum : "201540209"
       }
     }
-  }
+
+}
   render() {
     return (
       <Container>
         <TopDiv>
           <ImgDiv>
-             <img src={this.state.productDetail.img} alt="product1"/>
+            <img src={this.state.productDetail.img} alt="product1" />
           </ImgDiv>
           <TextDiv>
             <Title>{this.state.productDetail.title}</Title>
-            <Price>{ this.state.productDetail.price }<div>원</div></Price>
+            <Price>{this.state.productDetail.price}<div>원</div></Price>
             <HR />
             <ProductState>
               <StateDiv>
                 <StateName>상품상태 </StateName>
-                <StateContent>{ this.state.productDetail.productState }</StateContent>
+                <StateContent>{this.state.productDetail.productState}</StateContent>
               </StateDiv>
               <StateDiv>
                 <StateName>교환여부 </StateName>
-                <StateContent>{ this.state.productDetail.trade }</StateContent>
+                <StateContent>{this.state.productDetail.trade}</StateContent>
               </StateDiv>
               <StateDiv>
                 <StateName>배송비 </StateName>
-                <StateContent space>{ this.state.productDetail.delivery }</StateContent>
+                <StateContent space>{this.state.productDetail.delivery}</StateContent>
               </StateDiv>
               <StateDiv>
                 <StateName>거래지역 </StateName>
-                <StateContent>{ this.state.productDetail.area }</StateContent>
+                <StateContent>{this.state.productDetail.area}</StateContent>
               </StateDiv>
             </ProductState>
             <BtnDiv>
-            <MessageBtn>쪽지 보내기</MessageBtn>
-          </BtnDiv>
+              <MessageBtn>쪽지 보내기</MessageBtn>
+            </BtnDiv>
           </TextDiv>
         </TopDiv>
+
+
+          <ProductDiv>
+          <MyInfo>
+            <Mydiv>
+              판매자 정보
+            </Mydiv>
+            <LogoDiv>
+            <Logo>
+              D
+            </Logo>
+              <Sellerstate>
+              <StateContent><div>{this.state.sellerinfo.id}</div></StateContent>
+              </Sellerstate>
+              </LogoDiv>
+              <Sellerstate>
+              <SellerContent>이름 : </SellerContent>
+              <StateContent>{this.state.sellerinfo.name}</StateContent>
+              </Sellerstate>
+              <Sellerstate>
+              <SellerContent>학과 : </SellerContent>  
+              <StateContent>{this.state.sellerinfo.department}</StateContent>
+              </Sellerstate>  
+              <Sellerstate>
+              <SellerContent>학번 : </SellerContent>
+              <StateContent>{this.state.sellerinfo.schoolnum}</StateContent>
+              </Sellerstate> 
+
+          </MyInfo>
+
+            <DetailAll>
+            <DetailDiv>
+              <H4>상품 정보</H4>
+              <img src={this.state.productDetail.img} alt="product2" />
+              <img src={this.state.productDetail.img} alt="product3" />
+              <StateContent><div>{this.state.productDetail.info }</div></StateContent>
+            </DetailDiv>
+            </DetailAll>
+
+          </ProductDiv>
+
       </Container>
     )
   }
 }
 
-const Container = styled.div `
+const Container = styled.div`
   margin: 0 auto;
   width: 1200px;
-
 `
-const TopDiv = styled.div `
+const TopDiv = styled.div`
   width: 1200px;
   height: 496px;
   background-color : white;
@@ -99,7 +146,6 @@ const Price = styled.div`
   font-weight: bold;
   display: flex;
   width: 100%;
-
   >div {
     font-size: 24px;
     line-height: 64px;
@@ -107,7 +153,7 @@ const Price = styled.div`
   }
 `
 
-const HR = styled.hr `
+const HR = styled.hr`
   border-color: #D2D2D2;
 `
 
@@ -132,13 +178,18 @@ const StateContent = styled.div`
       margin-left: 16px;
     `
   }
+    >div {
+    font-size : 20px;
+    text-align : left;
+    margin-left : 30px;
+    }
 `
 
 const BtnDiv = styled.div`
   padding: 20px;
 `
 
-const MessageBtn = styled.button `
+const MessageBtn = styled.button`
   width: 240px;
   height: 60px;
   background: #0095C8;
@@ -149,5 +200,78 @@ const MessageBtn = styled.button `
   font-size: 20px;
   font-weight: bold;
 `
+const ProductDiv = styled.div` 
+  
+`
+const DetailAll = styled.div` 
+  width: 856px;
+  height : 1300px;
+  background-color : white;
+  display: flex;
+  margin-top : 40px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 30px;
+  
+`
+const H4 = styled.h4` 
+    width : 321px;
+    height : 40px;
+    font-size : 20px;
+    font-weight : bold;
+    margin : 40px 0 0 40px;
+    text-align : left;
+`
 
-export default Detail;
+const DetailDiv = styled.div` 
+  >img {
+      width : 783px;
+      height : 472px;
+      border-radius: 30px;
+      padding :20px
+    }
+`
+const MyInfo = styled.div` 
+  width: 301px;
+  height : 400px;
+  background-color : white;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 30px;
+  float : right;
+  padding-bottom : 20px;
+`
+const Logo = styled.h5`
+  width: 84px;
+  height: 84px;
+  color: white;
+  font-size: 50px;
+  line-height : 78px;
+  background-color: #0095c8;
+  border: 1px solid;
+  border-radius: 60px;
+  margin: 0 0 20px 0;
+`;
+const LogoDiv = styled.div ` 
+  display : flex;
+  margin-left: 20px;
+`
+const Mydiv = styled.div ` 
+  font-size : 20px;
+  font-weight : bold;
+  padding : 20px;
+  margin : 20px 0 10px 0;
+  display: flex;
+`
+
+const Sellerstate = styled.div`
+  display: flex;
+  padding : 15px;
+  margin-left : 15px;
+  margin-top : 10px;
+  font-weight : bold;
+`
+
+const SellerContent = styled.div`
+  margin-right: 12px;
+  font-weight : bold;
+  `
+export default Detail; 
